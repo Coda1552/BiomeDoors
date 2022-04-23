847ce1970b51ec2a8fc758e628600b34afb3e5b8
Cody <cody.koelliker@outlook.com>
2022-04-23 11:03:02 -0600

    add todo

--- src/main/java/coda/biomedoors/common/BiomeListSavedData.java
+++ src/main/java/coda/biomedoors/common/BiomeListSavedData.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+// todo - exiting/entering a world does not save nbt
 public class BiomeListSavedData extends SavedData {
     private Map<Block, ResourceKey<Biome>> map;
 
